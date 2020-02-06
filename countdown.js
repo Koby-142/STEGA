03bfcafad6c29af16026e6ec5fc70ba9113629e4
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2020-02-06 09:09:26 -0600

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,23 @@
+function startCountDown(){
+    var timeLeft =5;
+    var elem = document.getElementById('countdown_div');
+    
+    var timerId = setInterval(countdown, 1000);
+    function doSomething(){
+    console.log("CountDown Complete");
+    document.getElementById('gamemusic').play();
+    start();
+    document.getElementById('cntd').style.display = "none";
+    window.navigator.vibrate(600);
+    }
+    function countdown() {
+      if (timeLeft == 0) {
+        clearTimeout(timerId);
+        doSomething();
+      } else {
+        elem.innerHTML = timeLeft;
+        timeLeft--;
+      }
+    }
+  }
+  
---
+++ timing.js
@@ -0,0 +1,36 @@
+function start(){
+  var timeLeft =50;
+  var elem = document.getElementById('time_div');
+  
+  var timerId = setInterval(countdown, 1000);
+  function doSomething(){
+
+  console.log("Time Complete");
+  document.getElementById("timeUp").style.display="block";
+  document.getElementById("end").style.display="block";
+  document.getElementById('gamemusic').pause();
+  document.getElementById('speedup').pause(); 
+  document.getElementById('endgame').play();
+  window.navigator.vibrate(600);
+ 
+  }
+
+  function warning(){
+    document.getElementById('gamemusic').pause(); 
+    document.getElementById('speedup').play(); 
+    document.getElementById("time_div").style.fontSize = "70px";
+  }
+
+  function countdown() {
+    if (timeLeft == 0) {
+      clearTimeout(timerId);
+      doSomething();
+    } else {
+      elem.innerHTML = timeLeft + ' Seconds';
+      timeLeft--;
+    }
+    if (timeLeft < 15) {
+      warning();
+    }
+  }
+}
