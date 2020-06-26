020367bf35fd0b02494d7359183e6feaa6ecf757
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2020-06-26 10:17:37 -0500

    stega updates

--- countdown.js
+++ countdown.js
@@ -14,6 +14,35 @@ function startCountDown(){
       if (timeLeft == 0) {
         clearTimeout(timerId);
         doSomething();
+        
+      window.addEventListener('deviceorientation', function(event) {
+
+        if (event.gamma < 1 && event.gamma > 2){
+        
+          plusSlides(1, 0);window.navigator.vibrate(75);up();status();
+
+
+        }
+        var gameStatus = 0;
+        function status(){
+          gameStatus += 1
+          console.log("Game Status:" + gameStatus);
+        }
+
+        if (gameStatus > 8){
+          console.log('Time Complete Via User Input');
+      document.getElementById('timeUp').style.display='block';
+      document.getElementById('end').style.display='block';
+      document.getElementById('gamemusic').pause()
+        }
+        document.getElementById("gyro").innerHTML = event.gamma;
+
+      //  if (event.beta < -50){
+         
+    //      plusSlides(1, 0);window.navigator.vibrate(50);down();document.getElementById//('I1').style.color = 'red';
+       // }
+        
+        });
       } else {
         elem.innerHTML = timeLeft;
         timeLeft--;
