5311f5c7ead8af273a377dd20675c559daf138ad
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2020-06-26 13:28:11 -0500

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -22,7 +22,7 @@ function startCountDown(){
        
       }
 
-      if (gameStatus > 10){
+      if (gameStatus > 20){
         plusSlides(1, 0);window.navigator.vibrate(75);status();
      
       }
