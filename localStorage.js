9564e7d35f47fae3cb5059b103f395f15d90c27d
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2020-06-27 16:51:40 -0500

    music Choice Update

---
+++ localStorage.js
@@ -0,0 +1,10 @@
+
+
+    var musicStatus = localStorage.getItem('currentMusic');
+document.getElementById('gamemusic').src = musicStatus;
+console.log("Music Set To:" + musicStatus);
+var musicStateE =localStorage.getItem('musicState')
+document.getElementById(musicStateE).checked = true;
+
+
+  
