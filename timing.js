4d745a196b6889986a12cdd0bd8586d7f02242aa
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2020-07-18 12:20:47 -0500

    Update timing.js

--- timing.js
+++ timing.js
@@ -12,6 +12,30 @@ function disableOrientation(){
         
 }
 
+  function doSomething(){
+    console.log("Time Complete");
+    document.getElementById("timeUp").style.display="block";
+    document.getElementById("end").style.display="block";
+    document.getElementById('gamemusic').pause();
+    document.getElementById('speedup').pause(); 
+    document.getElementById('endgame').play(); 
+    window.navigator.vibrate(600);
+    metaGame = 10000000;
+    gameStatus = 0;
+
+    document.getElementById('m8').style.display="none";
+    document.getElementById('m7').style.display="none";
+    document.getElementById('m6').style.display="none";
+    document.getElementById('m5').style.display="none";
+    document.getElementById('m4').style.display="none";
+    document.getElementById('m3').style.display="none";
+    document.getElementById('m2').style.display="none";
+    document.getElementById('m1').style.display="none";
+    document.getElementById('countdownScript').src="null"; 
+    document.getElementById('time').src="null"; 
+    window.removeEventListener('deviceorientation', deviceOrientation); 
+  
+  }
 
   function warning(){
     document.getElementById('gamemusic').pause(); 
@@ -22,6 +46,7 @@ function disableOrientation(){
   function countdown() {
     if (timeLeft == 0) {
       clearTimeout(timerId);
+      doSomething();
   
     } else {
       elem.innerHTML = timeLeft + ' Seconds';
