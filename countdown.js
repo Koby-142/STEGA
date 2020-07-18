8b63aff97e97b350d92892d314533fa48132102d
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2020-07-18 12:22:51 -0500

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -42,33 +42,30 @@ function startCountDown(){
           globalGameStatus += 1;
         }
 
-        while (globalGameStatus < 8){
-          
-          if (globalGameStatus > 8){
-            console.log("Time Complete");
-            document.getElementById("timeUp").style.display="block";
-            document.getElementById("end").style.display="block";
-            document.getElementById('gamemusic').pause();
-            document.getElementById('speedup').pause(); 
-            document.getElementById('endgame').play(); 
-            window.navigator.vibrate(600);
-            metaGame = 10000000;
-            gameStatus = 0;
-            document.getElementById('m8').style.display="none";
-            document.getElementById('m7').style.display="none";
-            document.getElementById('m6').style.display="none";
-            document.getElementById('m5').style.display="none";
-            document.getElementById('m4').style.display="none";
-            document.getElementById('m3').style.display="none";
-            document.getElementById('m2').style.display="none";
-            document.getElementById('m1').style.display="none";
-            document.getElementById('countdownScript').src="null"; 
-            document.getElementById('time').src="null";  
-            break;
-          }
+    
+    
+        if (globalGameStatus > 8){
+          console.log("Time Complete");
+          document.getElementById("timeUp").style.display="block";
+          document.getElementById("end").style.display="block";
+          document.getElementById('gamemusic').pause();
+          document.getElementById('speedup').pause(); 
+          document.getElementById('endgame').play(); 
+          window.navigator.vibrate(600);
+          metaGame = 10000000;
+          gameStatus = 0;
+          document.getElementById('m8').style.display="none";
+          document.getElementById('m7').style.display="none";
+          document.getElementById('m6').style.display="none";
+          document.getElementById('m5').style.display="none";
+          document.getElementById('m4').style.display="none";
+          document.getElementById('m3').style.display="none";
+          document.getElementById('m2').style.display="none";
+          document.getElementById('m1').style.display="none";
+          document.getElementById('countdownScript').src="null"; 
+          document.getElementById('time').src="null";  
+          break;
         }
-    
-    
      
     
         document.getElementById("gyro").innerHTML = event.gamma;
