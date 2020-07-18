22941e59004ce08907eb9676a19321377225d83b
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2020-07-18 12:18:36 -0500

    more bug fixes

--- countdown.js
+++ countdown.js
@@ -41,30 +41,34 @@ function startCountDown(){
           gameStatus = 0;
           globalGameStatus += 1;
         }
-    
-        if (globalGameStatus > 8){
-          console.log("Time Complete");
-          document.getElementById("timeUp").style.display="block";
-          document.getElementById("end").style.display="block";
-          document.getElementById('gamemusic').pause();
-          document.getElementById('speedup').pause(); 
-          document.getElementById('endgame').play(); 
-          window.navigator.vibrate(600);
-          metaGame = 10000000;
-          gameStatus = 0;
-          document.getElementById('m8').style.display="none";
-          document.getElementById('m7').style.display="none";
-          document.getElementById('m6').style.display="none";
-          document.getElementById('m5').style.display="none";
-          document.getElementById('m4').style.display="none";
-          document.getElementById('m3').style.display="none";
-          document.getElementById('m2').style.display="none";
-          document.getElementById('m1').style.display="none";
-          document.getElementById('countdownScript').src="null"; 
-          document.getElementById('time').src="null"; 
-          window.removeEventListener('deviceorientation', deviceOrientation); 
-          break;
+
+        while (globalGameStatus < 8){
+          
+          if (globalGameStatus > 8){
+            console.log("Time Complete");
+            document.getElementById("timeUp").style.display="block";
+            document.getElementById("end").style.display="block";
+            document.getElementById('gamemusic').pause();
+            document.getElementById('speedup').pause(); 
+            document.getElementById('endgame').play(); 
+            window.navigator.vibrate(600);
+            metaGame = 10000000;
+            gameStatus = 0;
+            document.getElementById('m8').style.display="none";
+            document.getElementById('m7').style.display="none";
+            document.getElementById('m6').style.display="none";
+            document.getElementById('m5').style.display="none";
+            document.getElementById('m4').style.display="none";
+            document.getElementById('m3').style.display="none";
+            document.getElementById('m2').style.display="none";
+            document.getElementById('m1').style.display="none";
+            document.getElementById('countdownScript').src="null"; 
+            document.getElementById('time').src="null";  
+            break;
+          }
         }
+    
+    
      
     
         document.getElementById("gyro").innerHTML = event.gamma;
@@ -74,7 +78,7 @@ function startCountDown(){
     //      plusSlides(1, 0);window.navigator.vibrate(50);down();document.getElementById//('I1').style.color = 'red';
        // }
         
-        });
+      });
 
     } else {
       elem.innerHTML = timeLeft;
