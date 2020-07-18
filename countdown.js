a5f173d7bf8616a72c4ebdeeee06e1d5d900f63a
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2020-07-18 16:55:39 -0500

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -44,30 +44,6 @@ function startCountDown(){
 
 
     
-        if (globalGameStatus > 8){
-          console.log("Time Complete");
-          document.getElementById("timeUp").style.display="block";
-          document.getElementById("end").style.display="block";
-          document.getElementById('gamemusic').pause();
-          document.getElementById('speedup').pause(); 
-          document.getElementById('endgame').play(); 
-          window.navigator.vibrate(600);
-          metaGame = 10000000;
-          gameStatus = 0;
-          globalGameStatus = 0
-          document.getElementById('m8').style.display="none";
-          document.getElementById('m7').style.display="none";
-          document.getElementById('m6').style.display="none";
-          document.getElementById('m5').style.display="none";
-          document.getElementById('m4').style.display="none";
-          document.getElementById('m3').style.display="none";
-          document.getElementById('m2').style.display="none";
-          document.getElementById('m1').style.display="none";
-          document.getElementById('countdownScript').remove();
-          document.getElementById('time').remove() ;
-          window.removeEventListener('deviceorientation', deviceOrientation); 
-          
-        }
      
     
         document.getElementById("gyro").innerHTML = event.gamma;
@@ -82,6 +58,32 @@ function startCountDown(){
     } else {
       elem.innerHTML = timeLeft;
       timeLeft--;
+    }
+
+    
+    if (globalGameStatus > 8){
+      console.log("Time Complete");
+      document.getElementById("timeUp").style.display="block";
+      document.getElementById("end").style.display="block";
+      document.getElementById('gamemusic').pause();
+      document.getElementById('speedup').pause(); 
+      document.getElementById('endgame').play(); 
+      window.navigator.vibrate(600);
+      metaGame = 10000000;
+      gameStatus = 0;
+      globalGameStatus = 0
+      document.getElementById('m8').style.display="none";
+      document.getElementById('m7').style.display="none";
+      document.getElementById('m6').style.display="none";
+      document.getElementById('m5').style.display="none";
+      document.getElementById('m4').style.display="none";
+      document.getElementById('m3').style.display="none";
+      document.getElementById('m2').style.display="none";
+      document.getElementById('m1').style.display="none";
+      document.getElementById('countdownScript').remove();
+      document.getElementById('time').remove() ;
+      window.removeEventListener('deviceorientation', deviceOrientation); 
+      
     }
   }
 }
