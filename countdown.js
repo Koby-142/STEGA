7312616178c452191b197b900dc59e212074c5cc
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2020-07-19 14:04:11 -0500

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -31,7 +31,7 @@ function startCountDown(){
         }
      
     
-        var metaGame = 20;
+        var metaGame = 12;
     
         if (gameStatus > metaGame){
           plusSlides(1, 0);status();up();window.navigator.vibrate(75);
